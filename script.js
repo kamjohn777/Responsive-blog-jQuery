6378dfbc10a28024b50009cbb1f2eec1aab07d2f
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-04-29 20:19:27 -0500

    removed comments and formatted the files with prettier

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 // Filter Js
-$(document).ready(function() {
-    $('.filter-item').click(function() {
-    const value = $(this).attr('data-filter')
-    if (value == 'all') {
-        $('.post-box').show('1000')
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post-box").show("1000");
     } else {
-        $('.post-box').not('.' + value).hide('1000')
-        $('.post-box').filter('.' + value).show('1000')
+      $(".post-box")
+        .not("." + value)
+        .hide("1000");
+      $(".post-box")
+        .filter("." + value)
+        .show("1000");
     }
-    });
-    // Add active class on selected item
-    $('.filter-item').click(function() {
-        $(this).addClass('active-filter').siblings().removeClass('active-filter');
-    });
+  });
+  // Add active class on selected item
+  $(".filter-item").click(function () {
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
 
-    // Header Background Change on Scroll
-let header = document.querySelector('.header');
+  // Header Background Change on Scroll
+  let header = document.querySelector(".header");
 
-if (header) {
-    window.addEventListener('scroll', () => {
-        header.classList.toggle('shadow', window.scrollY > 0);
+  if (header) {
+    window.addEventListener("scroll", () => {
+      header.classList.toggle("shadow", window.scrollY > 0);
     });
-} else {
+  } else {
     console.error("Element with class 'header' not found");
-}
+  }
 });
